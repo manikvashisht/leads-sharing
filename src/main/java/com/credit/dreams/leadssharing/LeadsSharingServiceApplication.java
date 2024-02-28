1d5a085836f6c69cc0a64ed917a1036de2b224df
Manik Vashisht <manik1607@gmail.com>
2024-02-27 20:32:51 -0500

    Initial Commit

---
+++ src/main/java/com/credit/dreams/leadssharing/LeadsSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.credit.dreams.leadssharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsSharingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsSharingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credit/dreams/leadssharing/LeadsSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.dreams.leadssharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
